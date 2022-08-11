02fc8d1f4809a725bb0f2bd940354490d473ac9f
pok9198 <96594037+pok9198@users.noreply.github.com>
2022-08-11 17:37:10 +0700

    munohash
    sckrip php

---
+++ cfg.php
@@ -0,0 +1,9 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 11; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.40 Mobile Safari/537.36";
+
+
+$cookie = "Ref=1990; DataProject=1658681579; Lang=english; ci_session=86a4532c10c9c4b73fbf694b67a1477ce8348174";
+
+
+
